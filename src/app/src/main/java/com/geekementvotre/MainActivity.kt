041159e701abaaa00f4347d06505e27f77ebdfa7
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-04-28 19:46:23 +0200

    fix(mainactivity): remove 2 useless imports

--- src/app/src/main/java/com/geekementvotre/MainActivity.kt
+++ src/app/src/main/java/com/geekementvotre/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
