a47495fcc27742835f2ff76057966e8bbed66a02
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-05-04 20:39:53 +0200

    fix(color): update color

--- src/app/src/main/java/com/geekementvotre/ui/theme/Color.kt
+++ src/app/src/main/java/com/geekementvotre/ui/theme/Color.kt
@@ -8,6 +8,9 @@ val GeekWhite = Color(0xFFFFFFFF)      // Texte principal
 val GeekGold = Color(0xFFFBBD23)       // Couleur d'accent (Boutons, Titres)
 val GeekSubtitle = Color(0xFFB1B1C6)   // Couleur pour les sous-titres
 val GeekDarkGray = Color(0xFF1A1A1A)   // Fond des cartes (existant, à ajuster si besoin)
+val GeekCardBackground = Color(0xFF141416)
+val GeekInputBackground = Color(0xFF2C2C2E)
+val GeekError = Color(0xFFE57373)
 
 // Couleurs pour les modals (Gradient)
 val GeekModalStart = Color(0xFF805BA3)
