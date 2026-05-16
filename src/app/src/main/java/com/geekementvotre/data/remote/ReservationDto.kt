34d5fbff204d5f77d066547588ff9d153e68a8f4
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-05-16 18:32:15 +0200

    feat(dto): update reservation data

--- src/app/src/main/java/com/geekementvotre/data/remote/ReservationDto.kt
+++ src/app/src/main/java/com/geekementvotre/data/remote/ReservationDto.kt
@@ -9,10 +9,11 @@ data class ReservationDto(
     val email: String,
     val date_de_naissance: String,
     val genre: String,
-    val selected_game: String,
-    val nb_joueurs: Int,
-    val niveau_experience: String,
     val selected_date: String,
+    val nb_joueurs: Int,
     val selected_time: String,
+    val selected_game: String,
+    val adresse_session: String? = null,
+    val message_specifique: String? = null,
     val created_at: String? = null
 )
